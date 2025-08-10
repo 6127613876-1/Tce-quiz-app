0962a307dd0890626c64463e88e96db5b0e34778
6127613876-1 <gokulpriyan781@gmail.com>
2025-08-10 17:24:55 +0530

    version 1.0

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// src/App.js
+import React from 'react';
+import Home from './Home';
+import './App.css';
+import { Route,Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+
+export default App;
